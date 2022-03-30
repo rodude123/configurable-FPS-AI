d31b2842675ae2dff0115bca5b22a380f55c35d7
rodude123 <rodude123@gmail.com>
2022-03-30 18:04:10 +0100

    Created Unity package folder structure
    Signed-off-by: rodude123 <rodude123@gmail.com>

---
+++ Runtime/Enemy/Configurators/ZombieAIConfiguration.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using utilities;
+
+namespace Enemy.Configurators
+{
+	[CreateAssetMenu(menuName = "FPS AI System/Zombie AI Configuration", fileName = "zombieAI.asset")]
+	public class ZombieAIConfiguration : ScriptableObject
+	{
+		public string walkParameterName;
+
+		public string attackParameterName;
+
+		[Range(0.1f, 20f)]
+		[Tooltip("Attack Range of the enemy")]
+		public float attackRange;
+
+		[Range(0.1f, 20f)]
+		[Tooltip("Damage dealt to the player per hit")]
+		public float damage = 2f;
+
+		[Range(1f, 20f)]
+		[Tooltip("The attack delay of the enemy")]
+		public double attackDelay = 1d;
+
+		public string dieParameterName;
+	}
+}
